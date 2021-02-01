e01232e1b3edf6b3b0af92153cb4bca9ac992578
Rebecca Hickson <rebecca.l.hickson@gmail.com>
2021-02-01 17:43:05 -0500

    commit tree ad35e554700c3ea461c1c60854fbdb4213a95f10

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+var num1 = 31;
+var num2 = 2;
 
-// Write your code here
+var multiply = num1 * num2;
+
+var random = Math.floor(Math.random() * 10) + 1;
+
+var num3 = 11;
+var num4 = 7;
+
+var mod = num3 % num4;
+
+var max = Math.max(1, 20);
